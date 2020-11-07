60dc8f250f4592f46b6defd0c82c97f0abd5a961
Félix Étienne Labrie <fel.labrie@live.ca>
2020-11-07 17:07:42 -0500

    Initial

---
+++ Lab5-Lambdas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Lambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> intList = new List<int> { 6, 5, 4, 3, 2, 1, 7, 10, 1 };
+
+        }
+    }
+}
