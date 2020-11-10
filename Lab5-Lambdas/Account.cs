df2a81e20e9fb8c404920a3e661be3e99dc275e2
Félix Étienne Labrie <fel.labrie@live.ca>
2020-11-10 18:29:19 -0500

    Finished

--- Lab5-Lambdas/Account.cs
+++ Lab5-Lambdas/Account.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Lab5_Lambdas
 {
@@ -11,5 +12,12 @@ namespace Lab5_Lambdas
         public int id;
         public string name;
         public double balance;
+
+        public string toString()
+        {
+            return "ID : " + this.id + " Name : " + this.name + " Balance : " + balance.ToString("C", CultureInfo.CurrentCulture);
+        }
     }
+
+    
 }
--- Lab5-Lambdas/Program.cs
+++ Lab5-Lambdas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Lab5_Lambdas
 {
@@ -18,20 +19,36 @@ namespace Lab5_Lambdas
 
             List<int> underFive = intList.Where(x => x < 5).ToList();
 
+            Console.WriteLine("Q1 Members of the list under five: ");
+
+            underFive.ForEach(Console.WriteLine);
+
+            Console.WriteLine("______________________\n");
+
             // Q2 Lowest number
+
+            Console.WriteLine("Q2 Lowest number: ");
 
             int smallestNum = intList.Min();
 
+            Console.WriteLine(smallestNum);
+
+            Console.WriteLine("______________________\n");
+
             // Q3 Members lower than average of list
+
+            Console.WriteLine("Q3 Members lower than the average of the list: ");
 
             List<int> lessAvg = intList.Where(y => y < intList.Average()).ToList();
 
+            lessAvg.ForEach(Console.WriteLine);
 
-
+            Console.WriteLine("______________________\n");
 
             // Part 2
 
-            Dictionary<int, string> indexedList = new Dictionary<int, string>(); indexedList.Add(1, "Green Division");
+            Dictionary<int, string> indexedList = new Dictionary<int, string>(); 
+            indexedList.Add(1, "Green Division");
             indexedList.Add(2, "Yellow Division");
             indexedList.Add(3, "Blue Division");
             indexedList.Add(4, "Pink Division");
@@ -39,18 +56,42 @@ namespace Lab5_Lambdas
 
             // Q4 Records with blue
 
+            Console.WriteLine("Q4 Records with the word blue: ");
 
+            var recBlue = indexedList.Where(x => x.Value.Contains("Blue")).ToList();
+
+            foreach(var n in recBlue)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("______________________\n");
 
             // Q5 Records which color has more than 4 letters
 
+            Console.WriteLine("Q5 Colors with more than 4 letters: ");
 
+            var moreThan4 = indexedList.Where(y => y.Value.TrimEnd("Division".ToCharArray()).Length > 5);
+
+            foreach(var t in moreThan4)
+            {
+                Console.WriteLine(t);
+            }
+
+            Console.WriteLine("______________________\n");
 
             // Q6 Statement that converts to bank accounts where amount is 10,000x the ID
 
+            Console.WriteLine("Q6 Convert to bank accounts with ID x 10,000 = balance: ");
 
+            var accounts = indexedList.Select(x => new Account() { id = x.Key, name = x.Value, balance = x.Key * 10000 }).ToList();
 
+            foreach(var s in accounts)
+            {
+                Console.WriteLine(s.toString());
+            }
 
-
+            Console.WriteLine("______________________\n");
 
             // Part 3
 
@@ -63,13 +104,35 @@ namespace Lab5_Lambdas
 
             // Q7 Average of balances
 
+            Console.WriteLine("Q7 Average of the balances: ");
 
+            var averageAcc = accountList.Select(x => x.balance).Average();
+
+            Console.WriteLine("The average is " + averageAcc.ToString("C", CultureInfo.CurrentCulture));
+
+            Console.WriteLine("______________________\n");
 
             // Q8 Average value of balances where balance > 10,000
 
+            Console.WriteLine("Q8 Average balance of accounts with balance over 10,000: ");
 
+            List<Account> filtList = accountList.Where(x => x.balance > 10000).ToList();
+            var averageAccFilt = filtList.Select(x => x.balance).Average();
+
+            Console.WriteLine("The average balance for the accounts with over $10,000 is : " + averageAccFilt.ToString("C", CultureInfo.CurrentCulture));
+
+            Console.WriteLine("______________________\n");
 
             // Q9 Bank account where balance > 10,000
+
+            Console.WriteLine("Q9 Accounts with a balance of over $10,000: ");
+
+            foreach(var f in filtList)
+            {
+                Console.WriteLine(f.toString());
+            }
+
+            Console.WriteLine("______________________\n");
 
         }
 
